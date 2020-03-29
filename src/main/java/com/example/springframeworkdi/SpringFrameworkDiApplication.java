f3bced9258042ee549c93f678b1f5d42d0c20b17
Amit Thawait <amit17thawait@gmail.com>
2020-03-29 12:20:22 +0530

    Initial Commit - Project skeleton

---
+++ src/main/java/com/example/springframeworkdi/SpringFrameworkDiApplication.java
@@ -0,0 +1,13 @@
+package com.example.springframeworkdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFrameworkDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFrameworkDiApplication.class, args);
+	}
+
+}
